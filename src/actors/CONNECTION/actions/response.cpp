742e02f3d21b7af35c754a550df868496728b33b
Igor Goryachev <igor@tegia.ru>
2024-07-18 19:10:20 +0300

    Добавлено больше логирования

--- src/actors/CONNECTION/actions/response.cpp
+++ src/actors/CONNECTION/actions/response.cpp
@@ -1,4 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////////////////
+#undef _LOG_LEVEL_
+#define _LOG_LEVEL_ _LOG_DEBUG_
+#include <tegia/context/log.h>
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
 /**
 
 	\brief Отправка http-запроса клиенту
@@ -100,6 +107,8 @@ int CONNECTION::response(const std::shared_ptr<message_t> &message)
 				message->data.dump() +
 				"\r\n";					
 		
+			LDEBUG("CONNECTION " + this->name + "\n\n" + this->connection->content);
+
 			FCGX_PutStr(this->connection->content.c_str(), this->connection->content.size(),this->connection->req->out);
 			FCGX_Finish_r(this->connection->req);
 			return 200;
@@ -125,6 +134,8 @@ int CONNECTION::response(const std::shared_ptr<message_t> &message)
 					message->http["response"]["header"].get<std::string>() +
 					"\r\n{}\r\n";
 			
+			LDEBUG("CONNECTION " + this->name + "\n\n" + this->connection->content);
+
 			FCGX_PutStr(this->connection->content.c_str(), this->connection->content.size(),this->connection->req->out);
 			FCGX_Finish_r(this->connection->req);
 			return 434;
@@ -146,6 +157,8 @@ int CONNECTION::response(const std::shared_ptr<message_t> &message)
 					message->http["response"]["header"].get<std::string>() +
 					"\r\n{}\r\n";
 			
+			LDEBUG("CONNECTION " + this->name + "\n\n" + this->connection->content);
+
 			FCGX_PutStr(this->connection->content.c_str(), this->connection->content.size(),this->connection->req->out);
 			FCGX_Finish_r(this->connection->req);
 			return 400;
@@ -168,6 +181,8 @@ int CONNECTION::response(const std::shared_ptr<message_t> &message)
 					message->http["response"]["header"].get<std::string>() +
 					"\r\n{}\r\n";
 			
+			LDEBUG("CONNECTION " + this->name + "\n\n" + this->connection->content);
+
 			FCGX_PutStr(this->connection->content.c_str(), this->connection->content.size(),this->connection->req->out);
 			FCGX_Finish_r(this->connection->req);
 			return 403;
@@ -191,6 +206,8 @@ int CONNECTION::response(const std::shared_ptr<message_t> &message)
 					message->http["response"]["header"].get<std::string>() +
 					"\r\n{}\r\n";
 			
+			LDEBUG("CONNECTION " + this->name + "\n\n" + this->connection->content);
+
 			FCGX_PutStr(this->connection->content.c_str(), this->connection->content.size(),this->connection->req->out);
 			FCGX_Finish_r(this->connection->req);
 			return 404;
@@ -213,6 +230,8 @@ int CONNECTION::response(const std::shared_ptr<message_t> &message)
 					"Cache-Control: no-cache\r\n" +
 					message->http["response"]["header"].get<std::string>() +
 					"\r\n{}\r\n";
+			
+			LDEBUG("CONNECTION " + this->name + "\n\n" + this->connection->content);
 			
 			FCGX_PutStr(this->connection->content.c_str(), this->connection->content.size(),this->connection->req->out);
 			FCGX_Finish_r(this->connection->req);
